b71f472f144e47925539e0fbb562792a7d86eacc
Marcelo Montero <marcelo.montero.f@gmail.com>
2024-01-10 22:09:01 -0300

    test is added to the exercise Welcome to Java

---
+++ src/test/java/com/hackerrank/introduction/exercise01/SolutionTest.java
@@ -0,0 +1,32 @@
+package com.hackerrank.introduction.exercise01;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testMain() {
+        Solution.main(null);
+        assertEquals("Hello, World.\nHello, Java.\n", outContent.toString());
+    }
+}
