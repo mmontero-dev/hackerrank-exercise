a1889d22597be66e23470f4ff5ac66b82149e6fb
Marcelo Montero <marcelo.montero.f@gmail.com>
2024-01-10 22:36:58 -0300

    exercise Java Stdin and Stdout I is added and test

---
+++ src/main/java/com/hackerrank/introduction/exercise02/Solution.java
@@ -0,0 +1,15 @@
+package com.hackerrank.introduction.exercise02;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0;i < 3;i++) {
+            System.out.println(scanner.nextInt());
+        }
+        scanner.close();
+    }
+}
